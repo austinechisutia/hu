e1ac660480b828eed95dbadfc667a5188be9c566
austinechisutia <austineakhonya624@gmail.com>
2025-01-29 20:51:20 +0300

    break

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js";
+import {cart, removeFromCart} from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -38,7 +38,9 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary 
+                  js-delete-from-cart" 
+                  data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -96,6 +98,14 @@ cart.forEach((cartItem)=>{
 );
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
+
+
+document.querySelectorAll(".js-delete-from-cart")
+    .forEach((link)=>{
+        link.addEventListener("click", ()=>{
+           console.log(link.dataset.productId);
+        })
+    });  
 
 
 
