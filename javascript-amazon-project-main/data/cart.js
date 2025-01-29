529fadce78061648681fbca559e36fa05c97851c
austinechisutia <austineakhonya624@gmail.com>
2025-01-29 11:40:39 +0300

    here

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,6 +1,10 @@
-export const cart = [
-
-]
+export const cart = [{
+    productIdd: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1
+}, {
+    productId: "f8a3b6e7-9a6f-4f9b-8e4c-1e2b3d7f1f0c",
+    quantity: 2
+}];
 
 export function addToCart(productId){
     let martchingItem;
